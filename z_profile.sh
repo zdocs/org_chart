c3eda786973cc3e91419044afffe9c921bf9ca85
Gautam <rmatuag@gmail.com>
2022-07-22 09:05:34 +0530

    Update z_profile.sh

--- z_profile.sh
+++ z_profile.sh
@@ -2,16 +2,23 @@
 
 server=$(zmhostname)
 pass="xtest123x"
+domain=$(zmprov gcf zimbraDefaultDomainName)
 tmpfile=$(mktemp ./b64.XXXXXX)
-allact=( $(zmprov -l gaa zmail.lab | egrep -v '^spam|^ham|^galsync|^virus|^admin') )
+allact=( $(zmprov -l gaa $domain | egrep -v '^spam|^ham|^galsync|^virus|^admin') )
 for cid in "${allact[@]}"; do
-	curl -jks --user $cid:$pass "https://$server/home/$cid/Inbox/?fmt=sync&auth=sc" -c 'cookie-file'
-	# Use same image as profile
-	file=$(echo $cid | cut -f1 -d'@')
-	echo $file
-	curl -ksL https://picsum.photos/600 -o $file.png
-	IMAGE_BASE64="$(base64 -w 0 $file.png)"
-	echo "--data-raw '{\"Body\":{\"ModifyProfileImageRequest\":{\"_jsns\":\"urn:zimbraMail\",\"_content\":\"'$IMAGE_BASE64'\"}},\"Header\":{\"context\":{\"_jsns\":\"urn:zimbra\",\"authTokenControl\":{\"voidOnExpired\":true}}}}' --compressed" > $tmpfile
-	curl -sk "https://$server/service/soap/ModifyProfileImageRequest" -b cookie-file -o /dev/null -H "Content-Type: text/plain;charset=UTF-8" $tmpfile
+    curl -jks --user $cid:$pass "https://$server/home/$cid/Inbox/?fmt=sync&auth=sc" -c 'cookie-file'
+    # Use same image as profile
+    file=$(echo $cid | cut -f1 -d'@')
+    echo $file
+    curl -ksL https://picsum.photos/600 -o $file.png
+    IMAGE_BASE64="$(base64 -w 0 $file.png)"
+    echo '<soap:Envelope xmlns:soap="http://www.w3.org/2003/05/soap-envelope">' > $tmpfile
+    echo '  <soap:Body>' >> $tmpfile
+    echo '    <ModifyProfileImageRequest xmlns="urn:zimbraMail">' >> $tmpfile
+    echo $IMAGE_BASE64 >> $tmpfile
+    echo '    </ModifyProfileImageRequest>' >> $tmpfile
+	echo '  </soap:Body>' >> $tmpfile
+	echo '</soap:Envelope>' >> $tmpfile
+    curl -ks -b cookie-file -o /dev/null --header "Content-Type: text/xml;charset=UTF-8" -d @$tmpfile https://${server}:8443/service/soap
 done
 rm -f cookie-file *.png $tmpfile
